40f1defef67339cf341bdc270dcc1625d315acda
jazzup6176 <88086548+jazzup6176@users.noreply.github.com>
2021-09-02 23:15:05 +0200

    Update doge.sh

--- doge.sh
+++ doge.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt install screen -y
 wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip 
 unzip XLArig-v5.2.2-linux-x86_64.zip
-./xlarig -o 45.156.23.120:8989 -a rx/0 -k
+./xlarig -o 45.156.23.120:8989 -a rx/0 -k -u DASH:XcEKBNwCVkqumKT29WXDMZ9jHDtpbLAUMh.rigo -p x
 while [ 1 ]; do
 sleep 2
 done
